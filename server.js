7537aa3387c0c58163a9b734d17daabfdac03911
ha <ha@gmail.com>
2024-08-24 19:17:51 +0530

    j

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const initializeDatabase = require("./database");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://zuaipostsproject.netlify.app"],
+  })
+);
 
 let db;
 
