2d878c87ddb4a6d8891a2439d526193773b770b4
ha <ha@gmail.com>
2024-08-24 22:30:41 +0530

    post workinng

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ initializeDatabase()
 
 // Access
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://zuaipostsproject.netlify.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
